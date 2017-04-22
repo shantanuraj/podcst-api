96dd851b4289c860eb7f0d01336987489795249d
Shantanu Raj <s@sraj.me>
2017-04-23 03:34:04 +0530

    Enable cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,12 +5,17 @@
 'use strict';
 
 import { Server } from 'hapi';
+
 import config from './config';
+import {
+  enableCors,
+} from './exts';
 
 export const createServer = async () => {
   try {
     const server = new Server();
     server.connection(config);
+    server.ext('onPreResponse', enableCors);
 
     return server;
   } catch (err) {
