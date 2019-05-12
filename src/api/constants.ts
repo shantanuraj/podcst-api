b7ed1af06379f04ff574d6522796be71603ca9a1
Shantanu Raj <s@sraj.me>
2019-05-12 16:32:06 +0530

    Limit minimum number of top podcasts requested to 2

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -10,6 +10,12 @@
 export const DEFAULT_PODCASTS_COUNT = 30;
 
 /**
+ * Minimum number of podcasts wanted from iTunes feed api
+ */
+export const MIN_PODCASTS_COUNT = 2;
+
+
+/**
  * Maximium number of podcasts returned by iTunes feed api
  */
 export const MAX_PODCASTS_COUNT = 200;
--- src/api/top.ts
+++ src/api/top.ts
@@ -3,11 +3,24 @@ import * as Koa from 'koa';
 import { top as topData } from '../data';
 import { sendResponse } from '../utils/send-response';
 import { useHandler } from '../utils/use-handler';
-import { DEFAULT_PODCASTS_COUNT, MAX_PODCASTS_COUNT } from './constants';
+import { DEFAULT_PODCASTS_COUNT, MAX_PODCASTS_COUNT, MIN_PODCASTS_COUNT } from './constants';
+
+function validLimit (limitStr: string) {
+  let limit = parseInt(limitStr, 10) || DEFAULT_PODCASTS_COUNT;
+  limit = Math.min(
+    limit,
+    MAX_PODCASTS_COUNT
+  );
+  limit = Math.max(
+    limit,
+    MIN_PODCASTS_COUNT
+  );
+  return limit;
+}
 
 async function top(ctx: Koa.Context) {
   let { limit } = ctx.query;
-  limit = Math.min((limit && parseInt(limit, 10)) || DEFAULT_PODCASTS_COUNT, MAX_PODCASTS_COUNT);
+  limit = validLimit(limit);
   const res = await topData(limit);
   sendResponse(ctx, res);
 }
