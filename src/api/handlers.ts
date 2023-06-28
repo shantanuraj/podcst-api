37198ec3e0ee93470ff387d2950922929a480ab4
Shantanu Raj <s@sraj.me>
2023-06-28 05:32:12 +0200

    fix(url): Handle encoded urls

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -2,39 +2,28 @@
  * Handlers for server
  */
 
-'use strict';
+"use strict";
 
-import {
-  IReply,
-  Request,
-} from 'hapi';
+import { IReply, Request } from "hapi";
 
-import {
-  feed,
-  search,
-  top,
-} from '../data';
+import { feed, search, top } from "../data";
 
-import {
-  DEFAULT_PODCASTS_COUNT,
-  URL_REGEX,
-} from './constants';
+import { DEFAULT_PODCASTS_COUNT, URL_REGEX } from "./constants";
 
-import {
-  convertFeedToSearchResponse,
-} from './utils';
+import { convertFeedToSearchResponse } from "./utils";
 
 export default {
   search(request: Request, reply: IReply) {
     const term = request.query.term;
-    const res: Promise<App.PodcastSearchResult[]> = URL_REGEX.test(term) ?
-      feed(term).then(convertFeedToSearchResponse(term)) : search(term);
+    const res: Promise<App.PodcastSearchResult[]> = URL_REGEX.test(term)
+      ? feed(term).then(convertFeedToSearchResponse(term))
+      : search(term);
     reply(res);
   },
 
   popular(request: Request, reply: IReply) {
     const limit =
-      request.query.limit && parseInt(request.query.limit, 10) ||
+      (request.query.limit && parseInt(request.query.limit, 10)) ||
       DEFAULT_PODCASTS_COUNT;
     reply(top(limit));
   },
@@ -43,10 +32,11 @@ export default {
     const url = request.query.url;
     if (!url) {
       reply({
-        message: 'Missing param url',
+        message: "Missing param url",
       }).statusCode = 400;
     } else {
-      reply(feed(url));
+      const decodedUrl = decodeURIComponent(url);
+      reply(feed(decodedUrl));
     }
-  }
+  },
 };
